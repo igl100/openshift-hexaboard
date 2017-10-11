a8d2c6a893776ee2de59dbd7c6bc86efecbdafe5
igalvanl@redhat.com <igalvanl@redhat.com>
2017-10-11 16:37:13 -0500

    changed cors to return origin server

--- server/middleware.js
+++ server/middleware.js
@@ -24,7 +24,12 @@ module.exports = exports = {
   },
 
   cors: function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+	var origin = req.headers['origin'];
+	if(origin != null || origin.lenght > 0){
+		res.header('Access-Control-Allow-Origin', origin);
+	}else{
+    		res.header('Access-Control-Allow-Origin', '*');
+    }
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
